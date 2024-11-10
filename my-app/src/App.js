e09a8450988d93d9f4bf9711915a078d8d60993f
Jason Cai <jasonfleetwood123@gmail.com>
2024-11-10 15:02:43 -0800

    Merge branch 'main' of https://github.com/JasonCai0522/HackCamp24

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
+import {timer} from './timer';
+
+const timerState = {
+  notstarted : "NOT STARTED",
+  paused: 'PAUSED',
+  running: 'RUNNING'
+}
+
+const workState = {
+  work: 'WORK',
+  rest: 'REST' 
+}
+
+
 
 function Button() {
   return (
@@ -12,10 +27,58 @@ function Button() {
 }
 
 function App() {
+  const [gameState, setGameState] = useState(timerState.notstarted);
+  const [work, setWork] = useState(workState.work);
+  const [timerAmount, setTimerAmount] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+
+
+  useEffect(() => {
+    let interval;
+
+    const getTime = () => {
+      if (!(gameState === timerState.paused)) {
+        const time = timerAmount - 1;
+      setTimerAmount(time);
+      
+      setHours(Math.floor((timerAmount / (60 * 60)) % 24));
+      setMinutes(Math.floor((timerAmount / 60) % 60));
+      setSeconds(Math.floor(timerAmount % 60));
+      }
+    };
+
+    // Change the timerAmount based off if its a work session or rest session
+    if (gameState === timerState.notstarted) {
+       if (work === workState.work) {
+        setTimerAmount(1500)
+       } else if (work === workState.rest) {
+        setTimerAmount(300)
+       }
+       // Start updating the timer
+       interval = setInterval(() => getTime(timerAmount), 1000);
+    } //else if (gameState === timerState.paused){
+      // Stop the timer, 
+    
+      else if (gameState === timerState.running) {
+      // Start updating the timer, without changing the timerAmount
+      interval = setInterval(() => getTime(timerAmount), 1000);
+      // Check if timerAmount is equal to zero
+    }
+    
+    return () => clearInterval(interval);
+
+  }, [gameState, timerAmount, work])
+
+
   return (
     <div>
       <h1 style = {{ textAlign: 'center' }}>My Productivity Pal</h1>
-      <Button />
+      <h1>{seconds}</h1>
+      <h1>{minutes}</h1>
+      <h1>{hours}</h1>
     </div>
   );
 }

bc099923522aba64887e33989535104773415322
Jason Cai <jasonfleetwood123@gmail.com>
2024-11-10 16:14:34 -0800

    Updated timer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
 import { useState, useEffect } from 'react';
-import {timer} from './timer';
 import creature from './Images/licker.png'; //logic statement that changes the source of the image depending on time
 import hammer from './Images/hammer.png'; //shows up when you cancel a task. 
 import styles from './App.css';
+
 
 const timerState = {
   notstarted : "NOT STARTED",
@@ -16,53 +17,53 @@ const workState = {
   rest: 'REST' 
 }
 
-function Button() {
-  return (
-    <div className = "container">
-      <button className = "startButton">
-        Begin Hatching Process
-      </button>
-    </div>
-  );
-}
-const Image = () => (
-  <img
-    className={styles.image}
-    src="/Images/licker.png'"
-    alt="licker"
-    />
-)
+
 function App() {
   const [gameState, setGameState] = useState(timerState.notstarted);
   const [work, setWork] = useState(workState.work);
   const [timerAmount, setTimerAmount] = useState(0);
+
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
 
 
+    }
+
+    if (isActive) {
+      setIsActive(false)
+    } else {
+      setIsActive(true)
+    }
+  }
+
+  const cancelButton = () => {
+    setGameState(timerState.notstarted);
+    setIsActive(true);
+  }
 
   useEffect(() => {
     let interval;
 
     const getTime = () => {
-      if (!(gameState === timerState.paused)) {
+      if (gameState === timerState.running) {
         const time = timerAmount - 1;
       setTimerAmount(time);
-      
+      }
       setHours(Math.floor((timerAmount / (60 * 60)) % 24));
       setMinutes(Math.floor((timerAmount / 60) % 60));
       setSeconds(Math.floor(timerAmount % 60));
-      }
     };
 
     // Change the timerAmount based off if its a work session or rest session
     if (gameState === timerState.notstarted) {
        if (work === workState.work) {
-        setTimerAmount(1500)
+        setTimerAmount(20)
        } else if (work === workState.rest) {
-        setTimerAmount(300)
+        setTimerAmount(10)
        }
+       setIsActive(true)
+
        // Start updating the timer
        interval = setInterval(() => getTime(timerAmount), 1000);
     } //else if (gameState === timerState.paused){
@@ -72,27 +73,31 @@ function App() {
       // Start updating the timer, without changing the timerAmount
       interval = setInterval(() => getTime(timerAmount), 1000);
       // Check if timerAmount is equal to zero
+      if (timerAmount === 0) {
+        if (work === workState.work) {
+          setWork(workState.rest);
+          setCount(count + 1)
+          console.log(count)
+         } else if (work === workState.rest) {
+          setWork(workState.work);
+         }
+         setGameState(timerState.notstarted)
+         
+      }
     }
     
     return () => clearInterval(interval);
 
-  }, [gameState, timerAmount, work])
+  }, [gameState, timerAmount, work, count])
 
-
-  const [isActive, setIsActive] = useState(false);
-
-  const toggleButton = (event) => {
-    setIsActive(prevState => !prevState);  // Toggle the state
-    console.log('Button clicked:', event.target.className);
-
-  };
 
   return (
     <div>
       <h1 style = {{ textAlign: 'center' }}>My Productivity Pal</h1>
 
-      <div className = "container">    
+      <div>00 : {minutes} : {seconds} </div>
 
+      <div className = "container">    
         <button
         onClick={toggleButton}
         className={isActive ? 'startButton' : "pauseButton"}
@@ -100,15 +105,13 @@ function App() {
         </button>
         
         <button
+        onClick = {cancelButton}
         className='cancelButton'
         >Cancel Hatching Process?
         </button>
-
       </div>
     </div>  
-
   );
 }
-
 
 export default App;
